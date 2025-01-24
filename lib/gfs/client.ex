01f17ce0b5ec7967edb42d610469302ebf96825e
Faraaz Ahmad <faraaz98@live.com>
2025-01-24 19:54:46 +0000

    chore: add algo for appending to replicas

--- lib/gfs/client.ex
+++ lib/gfs/client.ex
@@ -15,4 +15,11 @@ defmodule Gfs.Client do
   def hello do
     :world
   end
+
+  def append_file(file_name, content) do
+    # get all chunks info from master
+    # Get last chunk and all its replicas
+    # Assign primary to one replica
+    # Append primary replica then append other secondary replicas
+  end
 end
